3f3fef21a65f216fd11ddac19ec9b8f6b7e1ee92
Hai Le <hai.le@foxsports.com.au>
2018-10-08 12:02:35 +1100

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Hai Le on 8/10/18.
+//  Copyright © 2018 Hai Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
